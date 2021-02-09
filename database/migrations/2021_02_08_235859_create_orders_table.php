decaf3e4ce6a2c94d20c49feef625bda1ea7e612
NimaCode <nimaghorbani315@yahoo.com>
2021-02-09 03:33:08 +0330

    add order model and migration

---
+++ database/migrations/2021_02_08_235859_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('food_id');
+            $table->timestamps();
+
+            $table->foreign('food_id')
+                ->references('id')->on('foods')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')->on('user_id')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
